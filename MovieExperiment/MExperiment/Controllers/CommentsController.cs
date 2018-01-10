787a470a98c89259daab9189c7d4e9f1f7d27faa
Jaime <itmanco@gmail.com>
2018-01-10 02:29:08 -0500

    Added Rates Controller

--- MovieExperiment/MExperiment/Controllers/CommentsController.cs
+++ MovieExperiment/MExperiment/Controllers/CommentsController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
 using MExperiment.Models;
 using System.Net.Http;
 using System.Net.Http.Headers;
---
+++ MovieExperiment/MExperiment/Controllers/RatesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MExperiment.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using MExperiment.Models.Enum;
+
+namespace MExperiment.Controllers
+{
+    public class RatesController : Controller
+    {
+        public RatesController()
+        {
+        }
+
+        public async Task<IActionResult> Create(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            @ViewData["SelectedMovie"] = id;
+            //TODO: Add user logged
+            @ViewData["userLogged"] = new Guid("0E984725-C51C-4BF4-9960-E1C80E27ABA0");
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdVideo,IdUser,Rate,DateTime")] Rates rate)
+        {
+            if (ModelState.IsValid)
+            {
+                rate.IdRate = Guid.NewGuid();
+                //Start
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Constants.BaseAPIAddress);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.PostAsJsonAsync("api/putrate?id=" + rate.IdVideo.ToString(), rate);
+
+                    
+                }
+            }
+            return RedirectToAction("Details", "Movies", new { id = rate.IdVideo.ToString() });
+            //--- END
+        }
+    }
+}
